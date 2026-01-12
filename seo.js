48339dfadeee686f2f7edc271e46f35a528fff73
504 <nexuscode.cs@gmail.com>
2026-01-12 14:38:49 +0300

    feat: add /new-file.txt

---
+++ seo.js
@@ -0,0 +1,115 @@
+const faviconSvg = `<svg fill="#000000" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="256px" height="256px" viewBox="-77.64 -77.64 709.84 709.84" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"><rect x="-77.64" y="-77.64" width="709.84" height="709.84" rx="49.6888" fill="#7ed0ec" strokewidth="0"></rect></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#ff" stroke-width="51.019428"> <g> <g> <path d="M531.14,226.158c-1.771-14.162-19.033-33.195-19.033-33.195c-5.752-44.26-20.804-20.359-20.804-20.359 c-18.147-13.28-15.491,17.262-15.491,17.262c-25.672,0-101.359,41.608-148.721,30.098c-47.36-11.509-129.243-1.326-143.408,3.982 s-36.736-0.886-47.801,9.294s-42.048,16.377-89.409,42.489c-47.356,26.116-46.471,50.016-46.471,50.016 c42.493,19.474,173.506-34.966,173.506-34.966c0,3.542,2.656,4.427,2.656,4.427c-12.395,4.868-24.345,19.033-33.195,20.359 c-8.854,1.326-11.065,5.753-11.065,5.753c-46.031,55.329-30.983,54.443-30.983,54.443c1.771,7.083,7.523,2.212,7.523,2.212 c3.982,0.885,5.753,2.211,9.739-8.854c3.986-11.065,23.901-41.608,51.788-42.049c27.883-0.444,44.26-10.18,44.26-10.18 c-28.772,27.887-30.539,13.721-27.442,29.653s18.148,36.736,23.023,40.278c4.875,3.541,33.191,0,33.191,0 c-4.868-7.083-19.474-4.721-19.474-4.721s-3.097-8.561-8.854-14.758c-5.753-6.197-11.95-24.786,16.818-27.887 s45.145-25.23,49.572-32.31c4.427-7.083,1.771-17.703,24.346-6.638c22.574,11.064,68.164,13.28,68.164,13.28 s-3.981,6.197,8.854,23.9c12.836,17.703,3.542,20.804-11.95,19.919c-15.491-0.886-15.047,14.161-15.047,14.161 c29.213,5.312,21.244-4.427,40.277-4.867s14.166-22.574,10.18-30.539c-3.981-7.968-5.752-24.345-5.752-24.345 s21.688-5.312,21.688,12.395s31.869,23.46,45.146,26.557c13.28,3.097,34.966,12.836,40.723,16.818 c5.757,3.981,19.033,19.033,28.768,12.835c9.739-6.197,12.836-14.606-1.771-15.932c-14.606-1.326-41.163-27.442-50.017-29.654 c-8.854-2.211-29.213-18.147-33.195-20.804c-3.981-2.656,2.212-19.918,4.868-27.001s36.296-42.489,53.113-25.23 c16.817,17.262,30.539,12.835,25.671,8.854c-4.867-3.982-20.208-22.575-20.208-22.575c31.579,25.672,41.894,9.739,39.237,7.524 C551.499,253.6,532.911,240.324,531.14,226.158z"></path> </g> </g> </g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M531.14,226.158c-1.771-14.162-19.033-33.195-19.033-33.195c-5.752-44.26-20.804-20.359-20.804-20.359 c-18.147-13.28-15.491,17.262-15.491,17.262c-25.672,0-101.359,41.608-148.721,30.098c-47.36-11.509-129.243-1.326-143.408,3.982 s-36.736-0.886-47.801,9.294s-42.048,16.377-89.409,42.489c-47.356,26.116-46.471,50.016-46.471,50.016 c42.493,19.474,173.506-34.966,173.506-34.966c0,3.542,2.656,4.427,2.656,4.427c-12.395,4.868-24.345,19.033-33.195,20.359 c-8.854,1.326-11.065,5.753-11.065,5.753c-46.031,55.329-30.983,54.443-30.983,54.443c1.771,7.083,7.523,2.212,7.523,2.212 c3.982,0.885,5.753,2.211,9.739-8.854c3.986-11.065,23.901-41.608,51.788-42.049c27.883-0.444,44.26-10.18,44.26-10.18 c-28.772,27.887-30.539,13.721-27.442,29.653s18.148,36.736,23.023,40.278c4.875,3.541,33.191,0,33.191,0 c-4.868-7.083-19.474-4.721-19.474-4.721s-3.097-8.561-8.854-14.758c-5.753-6.197-11.95-24.786,16.818-27.887 s45.145-25.23,49.572-32.31c4.427-7.083,1.771-17.703,24.346-6.638c22.574,11.064,68.164,13.28,68.164,13.28 s-3.981,6.197,8.854,23.9c12.836,17.703,3.542,20.804-11.95,19.919c-15.491-0.886-15.047,14.161-15.047,14.161 c29.213,5.312,21.244-4.427,40.277-4.867s14.166-22.574,10.18-30.539c-3.981-7.968-5.752-24.345-5.752-24.345 s21.688-5.312,21.688,12.395s31.869,23.46,45.146,26.557c13.28,3.097,34.966,12.836,40.723,16.818 c5.757,3.981,19.033,19.033,28.768,12.835c9.739-6.197,12.836-14.606-1.771-15.932c-14.606-1.326-41.163-27.442-50.017-29.654 c-8.854-2.211-29.213-18.147-33.195-20.804c-3.981-2.656,2.212-19.918,4.868-27.001s36.296-42.489,53.113-25.23 c16.817,17.262,30.539,12.835,25.671,8.854c-4.867-3.982-20.208-22.575-20.208-22.575c31.579,25.672,41.894,9.739,39.237,7.524 C551.499,253.6,532.911,240.324,531.14,226.158z"></path> </g> </g> </g></svg>`;
+
+const descriptions = [
+  "Am watching You",
+  "You cannot run",
+  "Come to me",
+  "The Internet is mine",
+  "I love you"
+];
+
+const svgDataUrl = `data:image/svg+xml;base64,${btoa(faviconSvg)}`;
+
+const getRandomDescription = () => {
+  return descriptions[Math.floor(Math.random() * descriptions.length)];
+};
+
+const updateTitleTime = () => {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+  const timeStr = `${hours} : ${minutes} : ${seconds}`;
+  
+  document.title = timeStr;
+  
+  const titleSelectors = [
+    'meta[name="title"]',
+    'meta[property="og:title"]',
+    'meta[name="twitter:title"]',
+    'meta[itemprop="name"]',
+    'meta[itemprop="headline"]',
+    'meta[name="application-name"]',
+    'meta[name="apple-mobile-web-app-title"]',
+    'meta[property="og:site_name"]',
+    'meta[name="parsely-title"]',
+    'meta[name="citation_title"]',
+    'meta[property="book:title"]',
+    'meta[property="product:name"]',
+    'meta[property="video:series"]',
+    'meta[property="article:section"]',
+    'meta[property="book:author"]',
+    'meta[property="profile:first_name"]',
+    'meta[property="profile:last_name"]'
+  ];
+  
+  titleSelectors.forEach(selector => {
+    const tag = document.querySelector(selector);
+    if (tag) tag.content = timeStr;
+  });
+};
+
+const updateFavicon = () => {
+  const faviconSelectors = [
+    'link[rel="icon"]',
+    'link[rel="shortcut icon"]',
+    'link[rel="apple-touch-icon"]',
+    'link[rel="mask-icon"]',
+    'meta[property="og:image"]',
+    'meta[name="twitter:image"]',
+    'meta[itemprop="image"]',
+    'meta[name="msapplication-TileImage"]'
+  ];
+  
+  faviconSelectors.forEach(selector => {
+    const tag = document.querySelector(selector);
+    if (tag) {
+      if (tag.hasAttribute('href')) {
+        tag.href = svgDataUrl;
+      }
+      if (tag.hasAttribute('content')) {
+        tag.content = svgDataUrl;
+      }
+    }
+  });
+  
+  let faviconLink = document.querySelector('link[rel="icon"]');
+  if (!faviconLink) {
+    faviconLink = document.createElement('link');
+    faviconLink.rel = 'icon';
+    faviconLink.type = 'image/svg+xml';
+    document.head.appendChild(faviconLink);
+  }
+  faviconLink.href = svgDataUrl;
+};
+
+const updateDescription = () => {
+  const randomDesc = getRandomDescription();
+  
+  const descSelectors = [
+    'meta[name="description"]',
+    'meta[property="og:description"]',
+    'meta[name="twitter:description"]',
+    'meta[itemprop="description"]'
+  ];
+  
+  descSelectors.forEach(selector => {
+    const tag = document.querySelector(selector);
+    if (tag) tag.content = randomDesc;
+  });
+};
+
+const init = () => {
+  updateTitleTime();
+  updateFavicon();
+  updateDescription();
+  
+  setInterval(updateTitleTime, 1000);
+  setInterval(updateDescription, 5000);
+};
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', init);
+} else {
+  init();
+}
